13e0492b0e304355681c6371b338be548624451b
Rusot <andreshm-14@outlook.com>
2024-08-14 02:55:50 -0600

    Actualizar app.py

--- app.py
+++ app.py
@@ -5,10 +5,12 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 # Configuración de la base de datos
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'  # Ruta del archivo de base de datos SQLite
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 #declaramos la base de datos
+
 db = SQLAlchemy(app)
 
 
@@ -22,20 +24,29 @@ class Data(db.Model):
     category = db.Column(db.String(50), nullable=False)
     minute_count = db.Column(db.Integer, default=0)
 
+# se configuran todas las acciones de los botones
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
+
     search_result = None
+
     if request.method == 'POST':
         if 'save' in request.form:
-            # al presionar el boton guardar, el dato se almacena en la matriz
+
+            # al presionar el boton guardar, el dato se almacena en la base de datos
+
             new_data = request.form.get('inse')
             category = request.form.get('category')
-            #toma el dato de la entrada de texto out, si hay dato lo arroja, en caso contrario se indica que no esta
+
+            #se toma el dato de la entrada de texto out, y lo almacena en la base de datos
+
             if not category:
                 search_result = "Por favor, selecciona una categoría válida."
             elif new_data:
                 existing_data = Data.query.filter_by(value=new_data).first()
                 if existing_data:
+
                     existing_data.timestamp = time.time()
                     existing_data.processed = False
                     existing_data.category = category
@@ -45,19 +56,22 @@ def index():
                     search_category = request.form.get('category')
                     new_entry = Data(value=new_data, timestamp=time.time(), category=category)
                     db.session.add(new_entry)   
-
-
-                    # Crear un nuevo registro si el dato no existe
+
                     
                 db.session.commit()
+
+
         if 'search' in request.form:
             
             # Buscar el dato en la base de datos
+
             search_query = request.form.get('out')            
             search_category = request.form.get('search_category')
         
 
             if search_query:
+
+
                 entry = Data.query.filter_by(value=search_query).first()
       
 
@@ -77,15 +91,18 @@ def index():
 
                         search_result = (
                             f"'{search_query}' encontrado en la categoría '{category}'. "
-                            f"Tiempo transcurrido: {elapsed_str}. Minutos contados: {minute_count}."
-                        )
+                            f"Tiempo transcurrido: {elapsed_str}. Minutos contados: {minute_count}.")
+
                     else:
+
                         search_result = f"'{search_query}' en la categoría '{search_category}' ya ha sido procesado anteriormente."
 
             else:
+
                 search_result = "Por favor, proporciona tanto el número como la categoría antes de buscar."
     
         if 'entrada' in request.form:
+
             search_in=request.form.get('nores')
             entry_2=Data.query.filter_by(value=search_in).first()
             
@@ -105,6 +122,7 @@ def index():
                     
                 
             else:
+
                 new_entry2 = Data(value=search_in, timestamp=time.time(),category="no residente")
                 db.session.add(new_entry2)
                 db.session.commit()
@@ -118,9 +136,11 @@ def index():
 
 if __name__ == '__main__':
     with app.app_context():
-        db.create_all()  # Crea las tablas en la base de datos
+
+        db.create_all()  
     app.run(debug=True)
+
 @app.route('/view_all')
 def view_all():
-    all_data = Data.query.all()  # Obtener todos los registros de la base de datos
+    all_data = Data.query.all()  
     return render_template('view_all.html', all_data=all_data)
